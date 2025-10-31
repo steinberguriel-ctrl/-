3ba15d68497f0cc1610080b74fffc09479958411
steinberguriel-ctrl <steinberguriel@gmail.com>
2025-10-31 16:14:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .catch(err => {
+        console.error("שגיאה בהוספה לקאש:", err);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
